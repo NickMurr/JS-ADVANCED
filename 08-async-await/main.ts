f333d97ceff572abcfbcd414a3033c24a93804b2
nickmurr <32221055+NickMurr@users.noreply.github.com>
2019-10-02 14:18:02 +0300

    async-await

--- 08-async-await/main.ts
+++ 08-async-await/main.ts
@@ -1,3 +1,23 @@
 const delay = (ms: number) => {
   return new Promise(resolve => setTimeout(() => resolve(), ms));
 };
+
+const fetchData = async (url: string) => {
+  console.log('Fetch started');
+  await delay(2000);
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return await data;
+  } catch (err) {
+    return { error: err };
+  } finally {
+    console.log('Done');
+  }
+};
+
+const d1 = fetchData(
+  'https://jsonplaceholder.typicode.com/todos?_limit=3'
+).then(da => {
+  console.log(da);
+});
